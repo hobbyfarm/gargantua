76e2441797cd09bc7acd47d5104c03b1bed4aaae
Bryton Hall <email@bryton.io>
2020-01-16 15:37:52 -0500

    vendor kubernetes/code-generator

--- generate-client.sh
+++ generate-client.sh
@@ -1,1 +1,5 @@
-./vendor/k8s.io/code-generator/generate-groups.sh all github.com/hobbyfarm/gargantua/pkg/client github.com/hobbyfarm/gargantua/pkg/apis "hobbyfarm.io:v1 terraformcontroller.cattle.io:v1"
+./vendor/k8s.io/code-generator/generate-groups.sh all \
+    github.com/hobbyfarm/gargantua/pkg/client \
+    github.com/hobbyfarm/gargantua/pkg/apis \
+    "hobbyfarm.io:v1 terraformcontroller.cattle.io:v1" \
+    --go-header-file ./hack/boilerplate.go.txt
--- go.mod
+++ go.mod
@@ -15,5 +15,6 @@ require (
 	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	k8s.io/code-generator v0.0.0-20190704094409-6c2a4329ac29
 	k8s.io/klog v0.3.0
 )
