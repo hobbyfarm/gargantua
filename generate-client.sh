59748de8b0854fbc76a6c643fdfc61ed1bfc5aa6
Luther Monson <luther.monson@gmail.com>
2020-01-17 10:59:10 -0700

    adding tools vendoring trick

--- generate-client.sh
+++ generate-client.sh
@@ -1,1 +1,6 @@
-./vendor/k8s.io/code-generator/generate-groups.sh all github.com/hobbyfarm/gargantua/pkg/client github.com/hobbyfarm/gargantua/pkg/apis "hobbyfarm.io:v1 terraformcontroller.cattle.io:v1"
+#!/bin/bash
+./vendor/k8s.io/code-generator/generate-groups.sh all \
+    github.com/hobbyfarm/gargantua/pkg/client \
+    github.com/hobbyfarm/gargantua/pkg/apis \
+    "hobbyfarm.io:v1 terraformcontroller.cattle.io:v1" \
+    --go-header-file ./hack/boilerplate.go.txt
