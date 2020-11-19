3d1acef675822e203e77d8add1f0887ab5b5711c
Eamon Bauman <eamon@eamonbauman.com>
2020-11-19 15:00:33 -0600

    moved admin course update functionality

--- pkg/courseserver/courseserver.go
+++ pkg/courseserver/courseserver.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hobbyfarm/gargantua/pkg/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/retry"
 	"net/http"
 	"strconv"
 )
@@ -57,7 +58,7 @@ func (c CourseServer) SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/a/course/{id}", c.GetCourse).Methods("GET")
 	r.HandleFunc("/a/course/list", c.ListFunc).Methods("GET")
 	r.HandleFunc("/a/course/new", c.CreateFunc).Methods("POST")
-
+	r.HandleFunc("/a/course/{id}", c.UpdateFunc).Methods("PUT")
 }
 
 func (c CourseServer) getPreparedCourseById(id string) (PreparedCourse, error) {
@@ -209,6 +210,103 @@ func (c CourseServer) CreateFunc(w http.ResponseWriter, r *http.Request) {
 
 	util.ReturnHTTPMessage(w, r, 201, "created", course.Name)
 	glog.V(4).Infof("Created course %s", course.Name)
+	return
+}
+
+func (c CourseServer) UpdateFunc(w http.ResponseWriter, r *http.Request) {
+	_, err := c.auth.AuthNAdmin(w, r)
+	if err != nil {
+		util.ReturnHTTPMessage(w, r, 403, "forbidden", "no access to update scenarios")
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+	if id == "" {
+		util.ReturnHTTPMessage(w, r, 400, "badrequest", "no id passed in")
+		return
+	}
+
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		course, err := c.hfClientSet.HobbyfarmV1().Courses().Get(id, metav1.GetOptions{})
+		if err != nil {
+			glog.Error(err)
+			util.ReturnHTTPMessage(w, r, 400, "badrequest", "no id found")
+			return fmt.Errorf("bad")
+		}
+		// name, description, scenarios, virtualmachines, keepaliveduration, pauseduration, pauseable
+
+		name := r.PostFormValue("name")
+		description := r.PostFormValue("description")
+		scenarios := r.PostFormValue("scenarios")
+		virtualMachinesRaw := r.PostFormValue("virtualmachines")
+		keepalive_duration := r.PostFormValue("keepalive_duration")
+		pause_duration := r.PostFormValue("pause_duration")
+		pauseableRaw := r.PostFormValue("pauseable")
+
+		if name != "" {
+			course.Spec.Name = name
+		}
+
+		if description != "" {
+			course.Spec.Description = description
+		}
+
+		if scenarios != "" {
+			scenarioSlice := make([]string, 0)
+			err = json.Unmarshal([]byte(scenarios), &scenarioSlice)
+			if err != nil {
+				glog.Errorf("error while unmarshalling scenarios %v", err)
+				util.ReturnHTTPMessage(w, r, 500, "internalerror", "error parsing")
+				return fmt.Errorf("bad")
+			}
+
+			course.Spec.Scenarios = scenarioSlice
+		}
+
+		if virtualMachinesRaw != "" {
+			virtualmachines := []map[string]string{}
+			err = json.Unmarshal([]byte(virtualMachinesRaw), &virtualmachines)
+			if err != nil {
+				glog.Errorf("error while unmarshaling VMs %v", err)
+				util.ReturnHTTPMessage(w, r, 500, "internalerror", "error parsing")
+				return fmt.Errorf("bad")
+			}
+
+			course.Spec.VirtualMachines = virtualmachines
+		}
+
+		if keepalive_duration != "" {
+			course.Spec.KeepAliveDuration = keepalive_duration
+		}
+
+		if pause_duration != "" {
+			course.Spec.PauseDuration = pause_duration
+		}
+
+		if pauseableRaw != "" {
+			pauseable, err := strconv.ParseBool(pauseableRaw)
+			if err != nil {
+				glog.Errorf("error while parsing bool: %v", err)
+				util.ReturnHTTPMessage(w, r, 500, "internalerror", "error parsing")
+				return fmt.Errorf("bad")
+			}
+
+			course.Spec.Pauseable = pauseable
+		}
+
+		_, updateErr := c.hfClientSet.HobbyfarmV1().Courses().Update(course)
+		return updateErr
+	})
+
+	if retryErr != nil {
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error attempting to update")
+		return
+	}
+
+	util.ReturnHTTPMessage(w, r, 200, "updated", "")
+	glog.V(4).Infof("Updated course %s", id)
 	return
 }
 
