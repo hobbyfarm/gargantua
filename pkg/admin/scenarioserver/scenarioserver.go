6ee49202c77882a227170650e4b3b6b7b7c975a9
Eamon Bauman <eamon@eamonbauman.com>
2020-11-19 15:42:46 -0600

    moved admin update func

--- pkg/admin/scenarioserver/scenarioserver.go
+++ pkg/admin/scenarioserver/scenarioserver.go
@@ -1,7 +1,6 @@
 package scenarioserver
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/golang/glog"
@@ -48,7 +47,6 @@ func (a AdminScenarioServer) getScenario(id string) (hfv1.Scenario, error) {
 }
 
 func (a AdminScenarioServer) SetupRoutes(r *mux.Router) {
-	r.HandleFunc("/a/scenario/{id}/printable", a.PrintFunc).Methods("GET")
 	r.HandleFunc("/a/scenario/{id}", a.UpdateFunc).Methods("PUT")
 	glog.V(2).Infof("set up routes for Scenario server")
 }
@@ -60,152 +58,8 @@ type PreparedScenario struct {
 
 
 
-func (a AdminScenarioServer) PrintFunc(w http.ResponseWriter, r *http.Request) {
-	_, err := a.auth.AuthNAdmin(w, r)
-	if err != nil {
-		util.ReturnHTTPMessage(w, r, 403, "forbidden", "no access to get Scenario")
-		return
-	}
-
-	vars := mux.Vars(r)
-
-	id := vars["id"]
-
-	if len(id) == 0 {
-		util.ReturnHTTPMessage(w, r, 500, "error", "no id passed in")
-		return
-	}
-
-	scenario, err := a.getScenario(id)
-
-	if err != nil {
-		glog.Errorf("error while retrieving scenario %v", err)
-		util.ReturnHTTPMessage(w, r, 500, "error", "no scenario found")
-		return
-	}
-
-	var content string
-
-	name, err := base64.StdEncoding.DecodeString(scenario.Spec.Name)
-	if err != nil {
-		glog.Errorf("Error decoding title of scenario: %s %v", scenario.Name, err)
-	}
-	description, err := base64.StdEncoding.DecodeString(scenario.Spec.Description)
-	if err != nil {
-		glog.Errorf("Error decoding description of scenario: %s %v", scenario.Name, err)
-	}
-
-	content = fmt.Sprintf("# %s\n%s\n\n", name, description)
-
-	for i, s := range scenario.Spec.Steps {
-
-		title, err := base64.StdEncoding.DecodeString(s.Title)
-		if err != nil {
-			glog.Errorf("Error decoding title of scenario: %s step %d: %v", scenario.Name, i, err)
-		}
-
-		content = content + fmt.Sprintf("## Step %d: %s\n", i+1, string(title))
-
-		stepContent, err := base64.StdEncoding.DecodeString(s.Content)
-		if err != nil {
-			glog.Errorf("Error decoding content of scenario: %s step %d: %v", scenario.Name, i, err)
-		}
-
-		content = content + fmt.Sprintf("%s\n", string(stepContent))
-	}
-
-	util.ReturnHTTPRaw(w, r, content)
-
-	glog.V(2).Infof("retrieved scenario and rendered for printability %s", scenario.Name)
-}
 
 
 
 
 
-func (a AdminScenarioServer) UpdateFunc(w http.ResponseWriter, r *http.Request) {
-	_, err := a.auth.AuthNAdmin(w, r)
-	if err != nil {
-		util.ReturnHTTPMessage(w, r, 403, "forbidden", "no access to update scenarios")
-		return
-	}
-
-	vars := mux.Vars(r)
-
-	id := vars["id"]
-	if id == "" {
-		util.ReturnHTTPMessage(w, r, 400, "badrequest", "no ID passed in")
-		return
-	}
-
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		scenario, err := a.hfClientSet.HobbyfarmV1().Scenarios().Get(id, metav1.GetOptions{})
-		if err != nil {
-			glog.Error(err)
-			util.ReturnHTTPMessage(w, r, 400, "badrequest", "no ID found")
-			return fmt.Errorf("bad")
-		}
-
-		name := r.PostFormValue("name")
-		description := r.PostFormValue("description")
-		rawSteps := r.PostFormValue("steps")
-		pauseable := r.PostFormValue("pauseable")
-		pause_duration := r.PostFormValue("pause_duration")
-		keepaliveDuration := r.PostFormValue("keepalive_duration")
-		rawVirtualMachines := r.PostFormValue("virtualmachines")
-
-		if name != "" {
-			scenario.Spec.Name = name
-		}
-		if description != "" {
-			scenario.Spec.Description = description
-		}
-		if keepaliveDuration != "" {
-			scenario.Spec.KeepAliveDuration = keepaliveDuration
-		}
-
-		if pauseable != "" {
-			if strings.ToLower(pauseable) == "true" {
-				scenario.Spec.Pauseable = true
-			} else {
-				scenario.Spec.Pauseable = false
-			}
-		}
-
-		if pause_duration != "" {
-			scenario.Spec.PauseDuration = pause_duration
-		}
-
-		if rawSteps != "" {
-			steps := []hfv1.ScenarioStep{}
-
-			err = json.Unmarshal([]byte(rawSteps), &steps)
-			if err != nil {
-				glog.Errorf("error while unmarshaling steps %v", err)
-				return fmt.Errorf("bad")
-			}
-			scenario.Spec.Steps = steps
-		}
-
-		if rawVirtualMachines != "" {
-			virtualmachines := []map[string]string{}
-			err = json.Unmarshal([]byte(rawVirtualMachines), &virtualmachines)
-			if err != nil {
-				glog.Errorf("error while unmarshaling VMs %v", err)
-				return fmt.Errorf("bad")
-			}
-			scenario.Spec.VirtualMachines = virtualmachines
-		}
-
-		_, updateErr := a.hfClientSet.HobbyfarmV1().Scenarios().Update(scenario)
-		return updateErr
-	})
-
-	if retryErr != nil {
-		util.ReturnHTTPMessage(w, r, 500, "error", "error attempting to update")
-		return
-	}
-
-	util.ReturnHTTPMessage(w, r, 200, "updated", "")
-	return
-}
--- pkg/scenarioserver/scenarioserver.go
+++ pkg/scenarioserver/scenarioserver.go
@@ -3,6 +3,7 @@ package scenarioserver
 import (
 	"crypto/sha256"
 	"encoding/base32"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/golang/glog"
@@ -15,6 +16,7 @@ import (
 	"github.com/hobbyfarm/gargantua/pkg/util"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/retry"
 	"net/http"
 	"strconv"
 	"strings"
@@ -68,7 +70,9 @@ func (s ScenarioServer) SetupRoutes(r *mux.Router) {
 	r.HandleFunc("/a/scenario/list", s.ListFunc).Methods("GET")
 	r.HandleFunc("/a/scenario/{id}", s.AdminGetFunc).Methods("GET")
 	r.HandleFunc("/scenario/{scenario_id}", s.GetScenarioFunc).Methods("GET")
+	r.HandleFunc("/a/scenario/{id}/printable", s.PrintFunc).Methods("GET")
 	r.HandleFunc("/a/scenario/new", s.CreateFunc).Methods("POST")
+	r.HandleFunc("/a/scenario/{id}", s.UpdateFunc).Methods("PUT")
 	r.HandleFunc("/scenario/{scenario_id}/step/{step_id:[0-9]+}", s.GetScenarioStepFunc).Methods("GET")
 	glog.V(2).Infof("set up route")
 }
@@ -279,6 +283,65 @@ func (s ScenarioServer) ListFunc(w http.ResponseWriter, r *http.Request) {
 	glog.V(2).Infof("listed scenarios")
 }
 
+func (s ScenarioServer) PrintFunc(w http.ResponseWriter, r *http.Request) {
+	_, err := s.auth.AuthNAdmin(w, r)
+	if err != nil {
+		util.ReturnHTTPMessage(w, r, 403, "forbidden", "no access to get Scenario")
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+
+	if len(id) == 0 {
+		util.ReturnHTTPMessage(w, r, 500, "error", "no id passed in")
+		return
+	}
+
+	scenario, err := s.GetScenarioById(id)
+
+	if err != nil {
+		glog.Errorf("error while retrieving scenario %v", err)
+		util.ReturnHTTPMessage(w, r, 500, "error", "no scenario found")
+		return
+	}
+
+	var content string
+
+	name, err := base64.StdEncoding.DecodeString(scenario.Spec.Name)
+	if err != nil {
+		glog.Errorf("Error decoding title of scenario: %s %v", scenario.Name, err)
+	}
+	description, err := base64.StdEncoding.DecodeString(scenario.Spec.Description)
+	if err != nil {
+		glog.Errorf("Error decoding description of scenario: %s %v", scenario.Name, err)
+	}
+
+	content = fmt.Sprintf("# %s\n%s\n\n", name, description)
+
+	for i, s := range scenario.Spec.Steps {
+
+		title, err := base64.StdEncoding.DecodeString(s.Title)
+		if err != nil {
+			glog.Errorf("Error decoding title of scenario: %s step %d: %v", scenario.Name, i, err)
+		}
+
+		content = content + fmt.Sprintf("## Step %d: %s\n", i+1, string(title))
+
+		stepContent, err := base64.StdEncoding.DecodeString(s.Content)
+		if err != nil {
+			glog.Errorf("Error decoding content of scenario: %s step %d: %v", scenario.Name, i, err)
+		}
+
+		content = content + fmt.Sprintf("%s\n", string(stepContent))
+	}
+
+	util.ReturnHTTPRaw(w, r, content)
+
+	glog.V(2).Infof("retrieved scenario and rendered for printability %s", scenario.Name)
+}
+
 func (s ScenarioServer) CreateFunc(w http.ResponseWriter, r *http.Request) {
 	_, err := s.auth.AuthNAdmin(w, r)
 	if err != nil {
@@ -359,6 +422,93 @@ func (s ScenarioServer) CreateFunc(w http.ResponseWriter, r *http.Request) {
 	}
 
 	util.ReturnHTTPMessage(w, r, 201, "created", scenario.Name)
+	return
+}
+
+func (s ScenarioServer) UpdateFunc(w http.ResponseWriter, r *http.Request) {
+	_, err := s.auth.AuthNAdmin(w, r)
+	if err != nil {
+		util.ReturnHTTPMessage(w, r, 403, "forbidden", "no access to update scenarios")
+		return
+	}
+
+	vars := mux.Vars(r)
+
+	id := vars["id"]
+	if id == "" {
+		util.ReturnHTTPMessage(w, r, 400, "badrequest", "no ID passed in")
+		return
+	}
+
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		scenario, err := s.hfClientSet.HobbyfarmV1().Scenarios().Get(id, metav1.GetOptions{})
+		if err != nil {
+			glog.Error(err)
+			util.ReturnHTTPMessage(w, r, 400, "badrequest", "no ID found")
+			return fmt.Errorf("bad")
+		}
+
+		name := r.PostFormValue("name")
+		description := r.PostFormValue("description")
+		rawSteps := r.PostFormValue("steps")
+		pauseable := r.PostFormValue("pauseable")
+		pause_duration := r.PostFormValue("pause_duration")
+		keepaliveDuration := r.PostFormValue("keepalive_duration")
+		rawVirtualMachines := r.PostFormValue("virtualmachines")
+
+		if name != "" {
+			scenario.Spec.Name = name
+		}
+		if description != "" {
+			scenario.Spec.Description = description
+		}
+		if keepaliveDuration != "" {
+			scenario.Spec.KeepAliveDuration = keepaliveDuration
+		}
+
+		if pauseable != "" {
+			if strings.ToLower(pauseable) == "true" {
+				scenario.Spec.Pauseable = true
+			} else {
+				scenario.Spec.Pauseable = false
+			}
+		}
+
+		if pause_duration != "" {
+			scenario.Spec.PauseDuration = pause_duration
+		}
+
+		if rawSteps != "" {
+			steps := []hfv1.ScenarioStep{}
+
+			err = json.Unmarshal([]byte(rawSteps), &steps)
+			if err != nil {
+				glog.Errorf("error while unmarshaling steps %v", err)
+				return fmt.Errorf("bad")
+			}
+			scenario.Spec.Steps = steps
+		}
+
+		if rawVirtualMachines != "" {
+			virtualmachines := []map[string]string{}
+			err = json.Unmarshal([]byte(rawVirtualMachines), &virtualmachines)
+			if err != nil {
+				glog.Errorf("error while unmarshaling VMs %v", err)
+				return fmt.Errorf("bad")
+			}
+			scenario.Spec.VirtualMachines = virtualmachines
+		}
+
+		_, updateErr := s.hfClientSet.HobbyfarmV1().Scenarios().Update(scenario)
+		return updateErr
+	})
+
+	if retryErr != nil {
+		util.ReturnHTTPMessage(w, r, 500, "error", "error attempting to update")
+		return
+	}
+
+	util.ReturnHTTPMessage(w, r, 200, "updated", "")
 	return
 }
 
