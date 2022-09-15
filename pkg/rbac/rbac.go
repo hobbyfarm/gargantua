007889fed1e00e5e52ca34fa9f6b98d057c6b699
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2022-09-15 19:19:15 +0200

    Fix rule for sessions

--- pkg/rbac/rbac.go
+++ pkg/rbac/rbac.go
@@ -37,14 +37,14 @@ func List() []Role{
 				addRule([] string {"hobbyfarm.io"}, [] string {"*"}, [] string {"scheduledevents", "accesscodes"}).
 			  	addRule([] string {"hobbyfarm.io"}, [] string {"list" , "get"}, [] string {"scenarios", "courses", "environments", "virtualmachinetemplates", "virtualmachinesets", "users"}).
 				addRule([] string {"hobbyfarm.io"}, [] string {"list" , "get", "watch"}, [] string {"progresses", "virtualmachines", "sessions"}).
-				addRule([] string {"hobbyfarm.io"}, [] string {"update" , "delete"}, [] string {"sessions"})
+				addRule([] string {"hobbyfarm.io"}, [] string {"update" , "delete", "list", "get"}, [] string {"sessions"})
 		}),
 		// ScheduledEvent Proctor is allowed to view scheduled events + dashboards
 		newRole("scheduledevent-proctor", func(r Role) Role {
 			return r.
 			  	addRule([] string {"hobbyfarm.io"}, [] string {"list" , "get"}, [] string {"scheduledevent","accesscodes", "scenarios", "courses", "environments", "virtualmachinetemplates", "virtualmachinesets", "users"}).
-				addRule([] string {"hobbyfarm.io"}, [] string {"list" , "get", "watch"}, [] string {"progresses", "virtualmachines", "sessions"}).
-				addRule([] string {"hobbyfarm.io"}, [] string {"update" , "delete"}, [] string {"sessions"})
+				addRule([] string {"hobbyfarm.io"}, [] string {"list" , "get", "watch"}, [] string {"progresses", "virtualmachines"}).
+				addRule([] string {"hobbyfarm.io"}, [] string {"update" , "delete", "list", "get"}, [] string {"sessions"})
 		}),
 		// User Manager can update and delete users
 		newRole("user-manager", func(r Role) Role {
