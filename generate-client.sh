3846c55a836a7295fc39998518bc3a9fbf3241ab
Eamon Bauman <eamon@eamonbauman.com>
2023-05-23 16:57:15 -0500

    Changed to using new kube_codegen.sh as recommended by upstream
    Signed-off-by: Eamon Bauman <eamon@eamonbauman.com>

--- generate-client.sh
+++ generate-client.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-$(pwd)/generate-groups.sh all \
-    github.com/hobbyfarm/gargantua/pkg/client \
-    github.com/hobbyfarm/gargantua/pkg/apis \
-    "hobbyfarm.io:v1,v2 terraformcontroller.cattle.io:v1" \
-    --go-header-file ./hack/boilerplate.go.txt
+source kube_codegen.sh
+
+mkdir -p pkg/client/clientset
+mkdir -p pkg/client/listers
+mkdir -p pkg/client/informers
+
+kube::codegen::gen_helpers \
+  --input-pkg-root github.com/hobbyfarm/gargantua/pkg/apis \
+  --output-base "${GOPATH}"/src
+
+kube::codegen::gen_client \
+  --input-pkg-root github.com/hobbyfarm/gargantua/pkg/apis \
+  --output-pkg-root github.com/hobbyfarm/gargantua/pkg/client \
+  --boilerplate ./hack/boilerplate.go.txt \
+  --output-base "${GOPATH}"/src \
+  --clientset-name clientset \
+  --versioned-name versioned \
+  --listers-name listers \
+  --informers-name informers \
+  --with-watch
