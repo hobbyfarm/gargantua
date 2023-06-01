a553d3cfa352478d367f43035a2d7edd2fa0203b
Eamon Bauman <eamon@eamonbauman.com>
2023-06-01 10:48:11 -0500

    fixed some json handling
    Signed-off-by: Eamon Bauman <eamon@eamonbauman.com>

--- pkg/property/types.go
+++ pkg/property/types.go
@@ -1,5 +1,12 @@
 package property
 
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type DataType string
 
 var (
@@ -21,7 +28,7 @@ var (
 
 type Property struct {
 	DataType  DataType  `json:"dataType"`
-	ValueType ValueType `json:"variableType"`
+	ValueType ValueType `json:"valueType"`
 
 	SettingValidation
 }
@@ -39,4 +46,77 @@ type SettingValidation struct {
 	Enum        []string
 	Default     *string
 	UniqueItems bool
+}
+
+// is there a more elegant way to solve this problem using generics?
+// probably not, i'll have two problems then.
+
+func (p Property) FromJSON(value string) (any, error) {
+	if p.ValueType == ValueTypeScalar {
+		switch p.DataType {
+		case DataTypeFloat:
+			return strconv.ParseFloat(value, 64)
+		case DataTypeInteger:
+			return strconv.Atoi(value)
+		case DataTypeBoolean:
+			return strconv.ParseBool(value)
+		case DataTypeString:
+			fallthrough
+		default:
+			return value, nil
+		}
+	}
+
+	decoder := json.NewDecoder(strings.NewReader(value))
+	decoder.DisallowUnknownFields()
+
+	if p.ValueType == ValueTypeMap {
+		switch p.DataType {
+		case DataTypeFloat:
+			var out = map[string]float64{}
+			return out, decoder.Decode(&out)
+		case DataTypeInteger:
+			var out = map[string]int{}
+			return out, decoder.Decode(&out)
+		case DataTypeString:
+			var out = map[string]string{}
+			return out, decoder.Decode(&out)
+		case DataTypeBoolean:
+			var out = map[string]bool{}
+			return out, decoder.Decode(&out)
+		}
+	}
+
+	if p.ValueType == ValueTypeArray {
+		switch p.DataType {
+		case DataTypeFloat:
+			var out = []float64{}
+			return out, decoder.Decode(&out)
+		case DataTypeInteger:
+			var out = []int{}
+			return out, decoder.Decode(&out)
+		case DataTypeString:
+			var out = []string{}
+			return out, decoder.Decode(&out)
+		case DataTypeBoolean:
+			var out = []bool{}
+			return out, decoder.Decode(&out)
+		}
+	}
+
+	return nil, fmt.Errorf("no match for datatype %s and valuetype %s", p.DataType, p.ValueType)
+}
+
+func (p Property) ToJSON(value string) ([]byte, error) {
+	switch p.ValueType {
+	case ValueTypeScalar:
+		switch p.DataType {
+		case DataTypeString:
+			return []byte("\"" + value + "\""), nil
+		default:
+			return []byte(value), nil
+		}
+	default:
+		return json.Marshal(value)
+	}
 }
