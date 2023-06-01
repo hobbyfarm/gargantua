ab0011a5a11ff8f58ba82c03d8278c38dfc5ed32
Eamon Bauman <eamon@eamonbauman.com>
2023-06-01 10:49:19 -0500

    fixed up handling
    Signed-off-by: Eamon Bauman <eamon@eamonbauman.com>

--- pkg/settingserver/settingserver.go
+++ pkg/settingserver/settingserver.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hobbyfarm/gargantua/pkg/rbacclient"
 	"github.com/hobbyfarm/gargantua/pkg/util"
 	"io"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
 	"strconv"
@@ -31,7 +32,7 @@ type SettingServer struct {
 type PreparedSetting struct {
 	Name string `json:"name"`
 	property.Property
-	Value  string `json:"value"`
+	Value  any    `json:"value"`
 	Scope  string `json:"scope"`
 	Weight int    `json:"weight"`
 }
@@ -47,8 +48,9 @@ func NewSettingServer(clientset hfClientset.Interface, authClient *authclient.Au
 }
 
 func (s SettingServer) SetupRoutes(r *mux.Router) {
-	r.HandleFunc("/setting/list/{scope}", s.ListFunc)
-	r.HandleFunc("/setting/update/{setting_id}", s.UpdateFunc)
+	r.HandleFunc("/setting/list/{scope}", s.ListFunc).Methods(http.MethodGet)
+	r.HandleFunc("/setting/update/{setting_id}", s.UpdateFunc).Methods(http.MethodPut)
+	r.HandleFunc("/setting/updatecollection", s.UpdateCollection).Methods(http.MethodPut)
 }
 
 func (s SettingServer) ListFunc(w http.ResponseWriter, r *http.Request) {
@@ -82,7 +84,14 @@ func (s SettingServer) ListFunc(w http.ResponseWriter, r *http.Request) {
 		weight, _ := ks.Labels[labels.SettingWeight]
 		iweight, _ := strconv.Atoi(weight)
 
-		settings = append(settings, PreparedSetting{Name: ks.Name, Property: ks.Property, Value: ks.Value, Scope: scope, Weight: iweight})
+		val, err := ks.Property.FromJSON(ks.Value)
+		if err != nil {
+			glog.Errorf("error encoding setting value for api: %s", err.Error())
+			util.ReturnHTTPMessage(w, r, 500, "internalerror", "error encoding setting as json")
+			return
+		}
+
+		settings = append(settings, PreparedSetting{Name: ks.Name, Property: ks.Property, Value: val, Scope: scope, Weight: iweight})
 	}
 
 	encodedSettings, err := json.Marshal(settings)
@@ -98,12 +107,6 @@ func (s SettingServer) ListFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s SettingServer) UpdateFunc(w http.ResponseWriter, r *http.Request) {
-	_, err := s.auth.AuthGrant(rbacclient.RbacRequest().HobbyfarmPermission(resourcePlural, rbacclient.VerbUpdate), w, r)
-	if err != nil {
-		util.ReturnHTTPMessage(w, r, 403, "forbidden", "no access to update settings")
-		return
-	}
-
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
 		glog.Errorf("error reading request body: %s", err.Error())
@@ -119,23 +122,91 @@ func (s SettingServer) UpdateFunc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// given the setting name, fetch it and update it
-	kSetting, err := s.hfClientSet.HobbyfarmV1().Settings(util.GetReleaseNamespace()).Get(s.ctx, setting.Name, metav1.GetOptions{})
-	if err != nil {
-		glog.Errorf("error getting setting: %s", err.Error())
-		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error getting setting from database")
-		return
-	}
-
-	kSetting.Value = setting.Value
-
-	_, err = s.hfClientSet.HobbyfarmV1().Settings(util.GetReleaseNamespace()).Update(s.ctx, kSetting, metav1.UpdateOptions{})
-	if err != nil {
-		glog.Errorf("error updating setting: %s", err.Error())
-		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error updating setting")
+	if ok := s.update(w, r, setting); !ok {
 		return
 	}
 
 	util.ReturnHTTPMessage(w, r, 200, "updated", "")
 	glog.V(8).Infof("updated setting %s", setting.Name)
+}
+
+func (s SettingServer) UpdateCollection(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		glog.Errorf("error reading request body: %s", err.Error())
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error handling request body")
+		return
+	}
+
+	settings := []PreparedSetting{}
+	err = json.Unmarshal(data, &settings)
+	if err != nil {
+		glog.Errorf("error unmarshalling json: %s", err.Error())
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error unmarshalling json body")
+		return
+	}
+
+	// for each setting, fetch it and update it
+	for _, set := range settings {
+		if ok := s.update(w, r, set); !ok {
+			return
+		}
+	}
+
+	util.ReturnHTTPMessage(w, r, 200, "updated", "")
+	glog.V(8).Info("updated settings")
+}
+
+func (s SettingServer) update(w http.ResponseWriter, r *http.Request, setting PreparedSetting) bool {
+	kSetting, err := s.hfClientSet.HobbyfarmV1().Settings(util.GetReleaseNamespace()).Get(s.ctx, setting.Name, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		util.ReturnHTTPMessage(w, r, 404, "notfound", "setting not found")
+		return false
+	}
+	if err != nil {
+		glog.Errorf("error getting setting from database: %s", err.Error())
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error updating setting")
+		return false
+	}
+
+	// check if the user has permissions to do this action
+	scope, ok := kSetting.Labels[labels.SettingScope]
+	if !ok {
+		glog.Errorf("setting %s does not have scope label", kSetting.Name)
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error updating setting")
+		return false
+	}
+
+	var resource = resourcePlural + "/" + scope
+	_, err = s.auth.AuthGrant(rbacclient.RbacRequest().HobbyfarmPermission(resource, rbacclient.VerbUpdate), w, r)
+	if err != nil {
+		util.ReturnHTTPMessage(w, r, 401, "forbidden", "no access to update setting")
+		return false
+	}
+
+	kSetting = kSetting.DeepCopy()
+
+	val, err := json.Marshal(setting.Value)
+	if err != nil {
+		glog.Errorf("error marshalling setting value: %s", err.Error())
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error updating setting")
+		return false
+	}
+
+	// validate the value
+	if err := kSetting.Validate(string(val)); err != nil {
+		util.ReturnHTTPMessage(w, r, http.StatusBadRequest, "badrequest", err.Error())
+		return false
+	}
+
+	kSetting.Value = string(val)
+
+	_, err = s.hfClientSet.HobbyfarmV1().Settings(util.GetReleaseNamespace()).Update(s.ctx, kSetting, metav1.UpdateOptions{})
+	if err != nil {
+		glog.Errorf("error updating setting: %s", err.Error())
+		util.ReturnHTTPMessage(w, r, 500, "internalerror", "error updating setting")
+		return false
+	}
+
+	return true
 }
