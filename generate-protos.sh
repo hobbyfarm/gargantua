e92785695205c0bd95476a9e49d6788ecef697e9
philipab <philip231001@googlemail.com>
2023-07-25 18:13:31 +0200

    Specify version (latest) for go proto/grpc plugins

--- generate-protos.sh
+++ generate-protos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROJECT_PATH="/go/src/github.com/hobbyfarm/gargantua"
+PROJECT_PATH="/github.com/hobbyfarm/gargantua"
 PROTO_DIR="${PROJECT_PATH}/protos"
 
 docker run --rm \
@@ -11,8 +11,8 @@ docker run --rm \
   golang:latest \
   -c "apt-get update && apt-get install -y protobuf-compiler && \
   export PATH=\$PATH:/go/bin && \
-  go install google.golang.org/protobuf/cmd/protoc-gen-go && \
-  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc && \
+  go install google.golang.org/protobuf/cmd/protoc-gen-go@latest && \
+  go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest && \
   find ${PROTO_DIR} -type f -name '*.proto' -exec bash -c ' \
     protoc -I ${PROTO_DIR} \
     --go_out=./protos \
