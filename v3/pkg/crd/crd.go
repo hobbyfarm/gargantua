6fd0f7023ebfa3223618dd37cdc91eabc3175df0
philipab <philip231001@googlemail.com>
2024-03-25 16:43:59 +0100

    Rm CRDs, add generic func for CRD installation
    The CRDs removed here are migrated towards their respective microservice

--- v3/pkg/crd/crd.go
+++ v3/pkg/crd/crd.go
@@ -1,64 +1,26 @@
 package crd
 
 import (
+	"os"
+
 	"github.com/ebauman/crder"
+	"github.com/golang/glog"
 	v1 "github.com/hobbyfarm/gargantua/v3/pkg/apis/hobbyfarm.io/v1"
 	terraformv1 "github.com/hobbyfarm/gargantua/v3/pkg/apis/terraformcontroller.cattle.io/v1"
+	"github.com/hobbyfarm/gargantua/v3/pkg/util"
+	"k8s.io/client-go/rest"
 )
+
+// A service managing a resource always needs to implement one of the following CRDInstaller interfaces
+type CrdInstaller interface {
+	GenerateCRDs() []crder.CRD
+}
+type CrdInstallerWithServiceReference interface {
+	GenerateCRDs(ca string, serviceReference ServiceReference) []crder.CRD
+}
 
 func GenerateCRDs() []crder.CRD {
 	return []crder.CRD{
-		HobbyfarmCRD(&v1.VirtualMachine{}, func(c *crder.CRD) {
-			c.
-				IsNamespaced(true).
-				AddVersion("v1", &v1.VirtualMachine{}, func(cv *crder.Version) {
-					cv.
-						WithColumn("Status", ".status.status").
-						WithColumn("Allocated", ".status.allocated").
-						WithColumn("PublicIP", ".status.public_ip").
-						WithColumn("PrivateIP", ".status.private_ip").
-						WithStatus()
-				})
-		}),
-		HobbyfarmCRD(&v1.Environment{}, func(c *crder.CRD) {
-			c.
-				IsNamespaced(true).
-				AddVersion("v1", &v1.Environment{}, nil)
-		}),
-		HobbyfarmCRD(&v1.Course{}, func(c *crder.CRD) {
-			c.
-				IsNamespaced(true).
-				AddVersion("v1", &v1.Course{}, nil)
-		}),
-		HobbyfarmCRD(&v1.Scenario{}, func(c *crder.CRD) {
-			c.
-				IsNamespaced(true).
-				AddVersion("v1", &v1.Scenario{}, nil)
-		}),
-		HobbyfarmCRD(&v1.Session{}, func(c *crder.CRD) {
-			c.
-				IsNamespaced(true).
-				AddVersion("v1", &v1.Session{}, func(cv *crder.Version) {
-					cv.
-						WithColumn("Paused", ".status.paused").
-						WithColumn("Active", ".status.active").
-						WithColumn("Finished", ".status.finished").
-						WithColumn("StartTime", ".status.start_time").
-						WithColumn("ExpirationTime", ".status.end_time").
-						WithStatus()
-				})
-		}),
-		HobbyfarmCRD(&v1.ScheduledEvent{}, func(c *crder.CRD) {
-			c.
-				IsNamespaced(true).
-				AddVersion("v1", &v1.ScheduledEvent{}, func(cv *crder.Version) {
-					cv.
-						WithColumn("AccessCode", ".spec.access_code").
-						WithColumn("Active", ".status.active").
-						WithColumn("Finished", ".status.finished").
-						WithStatus()
-				})
-		}),
 		HobbyfarmCRD(&v1.PredefinedService{}, func(c *crder.CRD) {
 			c.
 				IsNamespaced(true).
@@ -104,4 +66,31 @@ func HobbyfarmCRD(obj interface{}, customize func(c *crder.CRD)) crder.CRD {
 
 func terraformCRD(obj interface{}, customize func(c *crder.CRD)) crder.CRD {
 	return *crder.NewCRD(obj, "terraformcontroller.cattle.io", customize)
+}
+
+func InstallCrds[T CrdInstaller](crdInstaller T, cfg *rest.Config, resourceName string) {
+	crds := crdInstaller.GenerateCRDs()
+	installCRDsFunc(cfg, resourceName, crds)
+}
+func InstallCrdsWithServiceReference[T CrdInstallerWithServiceReference](crdInstaller T, cfg *rest.Config, resourceName string, webhookTlsCa string) {
+	ca, err := os.ReadFile(webhookTlsCa)
+	if err != nil {
+		glog.Fatalf("error reading ca certificate: %s", err.Error())
+	}
+
+	crds := crdInstaller.GenerateCRDs(string(ca), ServiceReference{
+		Namespace: util.GetReleaseNamespace(),
+		Name:      "hobbyfarm-webhook",
+	})
+
+	installCRDsFunc(cfg, resourceName, crds)
+}
+
+func installCRDsFunc(cfg *rest.Config, resourceName string, crds []crder.CRD) {
+	glog.Infof("installing/updating %s CRDs", resourceName)
+	err := crder.InstallUpdateCRDs(cfg, crds...)
+	if err != nil {
+		glog.Fatalf("failed installing/updating %s CRDs: %s", resourceName, err.Error())
+	}
+	glog.Infof("finished installing/updating %s CRDs", resourceName)
 }
