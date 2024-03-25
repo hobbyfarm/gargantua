a0a9a481082888420257ccb85577088ffc4a5599
philipab <philip231001@googlemail.com>
2024-03-25 17:24:02 +0100

    Add internal environment service

---
+++ v3/services/environmentsvc/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/hobbyfarm/gargantua/v3/pkg/crd"
+	"github.com/hobbyfarm/gargantua/v3/pkg/microservices"
+	"github.com/hobbyfarm/gargantua/v3/pkg/signals"
+	"github.com/hobbyfarm/gargantua/v3/pkg/util"
+
+	environmentservice "github.com/hobbyfarm/gargantua/services/environmentsvc/v3/internal"
+	hfInformers "github.com/hobbyfarm/gargantua/v3/pkg/client/informers/externalversions"
+	environmentProto "github.com/hobbyfarm/gargantua/v3/protos/environment"
+)
+
+var (
+	serviceConfig *microservices.ServiceConfig
+)
+
+func init() {
+	serviceConfig = microservices.BuildServiceConfig()
+}
+
+func main() {
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, hfClient, _ := microservices.BuildClusterConfig(serviceConfig)
+
+	namespace := util.GetReleaseNamespace()
+	hfInformerFactory := hfInformers.NewSharedInformerFactoryWithOptions(hfClient, time.Second*30, hfInformers.WithNamespace(namespace))
+
+	crd.InstallCrds(environmentservice.EnvironmentCRDInstaller{}, cfg, "environment")
+
+	gs := microservices.CreateGRPCServer(serviceConfig.ServerCert.Clone())
+
+	es := environmentservice.NewGrpcEnvironmentServer(hfClient, hfInformerFactory)
+	environmentProto.RegisterEnvironmentSvcServer(gs, es)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		microservices.StartGRPCServer(gs, serviceConfig.EnableReflection)
+	}()
+
+	hfInformerFactory.Start(stopCh)
+
+	wg.Wait()
+}
