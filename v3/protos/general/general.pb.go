ffd19d2f1f90b13d2e091b7b2b13d5da5d99a8c4
philipab <philip231001@googlemail.com>
2024-05-15 15:42:33 +0200

    Add wrapper type for string slices

--- v3/protos/general/general.pb.go
+++ v3/protos/general/general.pb.go
@@ -361,6 +361,54 @@ func (x *StringMap) GetValue() map[string]string {
 	return nil
 }
 
+// A wrapper for string slices in case we need to differ between an empty and unset slice
+type StringArray struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Values []string `protobuf:"bytes,1,rep,name=values,proto3" json:"values,omitempty"`
+}
+
+func (x *StringArray) Reset() {
+	*x = StringArray{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_general_general_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *StringArray) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*StringArray) ProtoMessage() {}
+
+func (x *StringArray) ProtoReflect() protoreflect.Message {
+	mi := &file_general_general_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use StringArray.ProtoReflect.Descriptor instead.
+func (*StringArray) Descriptor() ([]byte, []int) {
+	return file_general_general_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *StringArray) GetValues() []string {
+	if x != nil {
+		return x.Values
+	}
+	return nil
+}
+
 var File_general_general_proto protoreflect.FileDescriptor
 
 var file_general_general_proto_rawDesc = []byte{
@@ -408,11 +456,13 @@ var file_general_general_proto_rawDesc = []byte{
 	0x6c, 0x75, 0x65, 0x1a, 0x38, 0x0a, 0x0a, 0x56, 0x61, 0x6c, 0x75, 0x65, 0x45, 0x6e, 0x74, 0x72,
 	0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03,
 	0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01,
-	0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x42, 0x32, 0x5a,
-	0x30, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x68, 0x6f, 0x62, 0x62,
-	0x79, 0x66, 0x61, 0x72, 0x6d, 0x2f, 0x67, 0x61, 0x72, 0x67, 0x61, 0x6e, 0x74, 0x75, 0x61, 0x2f,
-	0x76, 0x33, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73, 0x2f, 0x67, 0x65, 0x6e, 0x65, 0x72, 0x61,
-	0x6c, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65, 0x3a, 0x02, 0x38, 0x01, 0x22, 0x25, 0x0a,
+	0x0b, 0x53, 0x74, 0x72, 0x69, 0x6e, 0x67, 0x41, 0x72, 0x72, 0x61, 0x79, 0x12, 0x16, 0x0a, 0x06,
+	0x76, 0x61, 0x6c, 0x75, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x09, 0x52, 0x06, 0x76, 0x61,
+	0x6c, 0x75, 0x65, 0x73, 0x42, 0x32, 0x5a, 0x30, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63,
+	0x6f, 0x6d, 0x2f, 0x68, 0x6f, 0x62, 0x62, 0x79, 0x66, 0x61, 0x72, 0x6d, 0x2f, 0x67, 0x61, 0x72,
+	0x67, 0x61, 0x6e, 0x74, 0x75, 0x61, 0x2f, 0x76, 0x33, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x73,
+	0x2f, 0x67, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x6c, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -427,7 +477,7 @@ func file_general_general_proto_rawDescGZIP() []byte {
 	return file_general_general_proto_rawDescData
 }
 
-var file_general_general_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
+var file_general_general_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_general_general_proto_goTypes = []interface{}{
 	(*ResourceId)(nil),           // 0: general.ResourceId
 	(*GetRequest)(nil),           // 1: general.GetRequest
@@ -435,14 +485,15 @@ var file_general_general_proto_goTypes = []interface{}{
 	(*OwnerReference)(nil),       // 3: general.OwnerReference
 	(*OwnerReferences)(nil),      // 4: general.OwnerReferences
 	(*StringMap)(nil),            // 5: general.StringMap
-	nil,                          // 6: general.StringMap.ValueEntry
-	(*wrapperspb.BoolValue)(nil), // 7: google.protobuf.BoolValue
+	(*StringArray)(nil),          // 6: general.StringArray
+	nil,                          // 7: general.StringMap.ValueEntry
+	(*wrapperspb.BoolValue)(nil), // 8: google.protobuf.BoolValue
 }
 var file_general_general_proto_depIdxs = []int32{
-	7, // 0: general.OwnerReference.controller:type_name -> google.protobuf.BoolValue
-	7, // 1: general.OwnerReference.block_owner_deletion:type_name -> google.protobuf.BoolValue
+	8, // 0: general.OwnerReference.controller:type_name -> google.protobuf.BoolValue
+	8, // 1: general.OwnerReference.block_owner_deletion:type_name -> google.protobuf.BoolValue
 	3, // 2: general.OwnerReferences.owner_references:type_name -> general.OwnerReference
-	6, // 3: general.StringMap.value:type_name -> general.StringMap.ValueEntry
+	7, // 3: general.StringMap.value:type_name -> general.StringMap.ValueEntry
 	4, // [4:4] is the sub-list for method output_type
 	4, // [4:4] is the sub-list for method input_type
 	4, // [4:4] is the sub-list for extension type_name
@@ -528,6 +579,18 @@ func file_general_general_proto_init() {
 				return nil
 			}
 		}
+		file_general_general_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*StringArray); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -535,7 +598,7 @@ func file_general_general_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_general_general_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   7,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
