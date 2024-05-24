f13e3fa64ff7adc627d3098437d11c0bb7afb1bf
philipab <philip231001@googlemail.com>
2024-05-24 16:37:12 +0200

    Add function to convert []*generalpb.StringMap to []map[string]string

--- v3/pkg/util/conversion.go
+++ v3/pkg/util/conversion.go
@@ -1,5 +1,9 @@
 package util
 
+import (
+	generalpb "github.com/hobbyfarm/gargantua/v3/protos/general"
+)
+
 type IntegerType interface {
 	uint32 | int
 }
@@ -9,6 +13,15 @@ func ConvertMap[T, U IntegerType](input map[string]T) map[string]U {
 	output := make(map[string]U)
 	for key, value := range input {
 		output[key] = U(value)
+	}
+	return output
+}
+
+// A function that converts []*generalpb.StringMap to []map[string]string
+func ConvertStringMapSlice(vms []*generalpb.StringMap) []map[string]string {
+	output := make([]map[string]string, 0, len(vms))
+	for _, vm := range vms {
+		output = append(output, vm.GetValue())
 	}
 	return output
 }
