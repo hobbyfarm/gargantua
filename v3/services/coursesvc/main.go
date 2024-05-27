d4ee45d4e0ddf648c0640ffc37bafaba88b98532
philipab <philip231001@googlemail.com>
2024-05-27 09:34:55 +0200

    Start api server via course service

--- v3/services/coursesvc/main.go
+++ v3/services/coursesvc/main.go
@@ -11,7 +11,13 @@ import (
 
 	courseservice "github.com/hobbyfarm/gargantua/services/coursesvc/v3/internal"
 	hfInformers "github.com/hobbyfarm/gargantua/v3/pkg/client/informers/externalversions"
+	accesscodepb "github.com/hobbyfarm/gargantua/v3/protos/accesscode"
+	authnpb "github.com/hobbyfarm/gargantua/v3/protos/authn"
+	authrpb "github.com/hobbyfarm/gargantua/v3/protos/authr"
 	coursepb "github.com/hobbyfarm/gargantua/v3/protos/course"
+	scenariopb "github.com/hobbyfarm/gargantua/v3/protos/scenario"
+	scheduledeventpb "github.com/hobbyfarm/gargantua/v3/protos/scheduledevent"
+	sessionpb "github.com/hobbyfarm/gargantua/v3/protos/session"
 )
 
 var (
@@ -32,6 +38,26 @@ func main() {
 
 	crd.InstallCrds(courseservice.CourseCRDInstaller{}, cfg, "course")
 
+	services := []microservices.MicroService{
+		microservices.AuthN,
+		microservices.AuthR,
+		microservices.AccessCode,
+		microservices.Scenario,
+		microservices.ScheduledEvent,
+		microservices.Session,
+	}
+	connections := microservices.EstablishConnections(services, serviceConfig.ClientCert)
+	for _, conn := range connections {
+		defer conn.Close()
+	}
+
+	authnClient := authnpb.NewAuthNClient(connections[microservices.AuthN])
+	authrClient := authrpb.NewAuthRClient(connections[microservices.AuthR])
+	acClient := accesscodepb.NewAccessCodeSvcClient(connections[microservices.AccessCode])
+	scenarioClient := scenariopb.NewScenarioSvcClient(connections[microservices.Scenario])
+	scheduledEventClient := scheduledeventpb.NewScheduledEventSvcClient(connections[microservices.ScheduledEvent])
+	sessionClient := sessionpb.NewSessionSvcClient(connections[microservices.Session])
+
 	gs := microservices.CreateGRPCServer(serviceConfig.ServerCert.Clone())
 
 	cs := courseservice.NewGrpcCourseServer(hfClient, hfInformerFactory)
@@ -43,6 +69,22 @@ func main() {
 	go func() {
 		defer wg.Done()
 		microservices.StartGRPCServer(gs, serviceConfig.EnableReflection)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		courseServer := courseservice.NewCourseServer(
+			authnClient,
+			authrClient,
+			acClient,
+			scenarioClient,
+			scheduledEventClient,
+			sessionClient,
+			cs,
+		)
+		microservices.StartAPIServer(courseServer)
 	}()
 
 	hfInformerFactory.Start(stopCh)
