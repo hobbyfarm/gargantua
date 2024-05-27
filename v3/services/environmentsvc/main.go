91d5d635639c286fab751bb2f909261988adb7e2
philipab <philip231001@googlemail.com>
2024-05-27 13:18:45 +0200

    Start environment api logic in environment service

--- v3/services/environmentsvc/main.go
+++ v3/services/environmentsvc/main.go
@@ -11,7 +11,10 @@ import (
 
 	environmentservice "github.com/hobbyfarm/gargantua/services/environmentsvc/v3/internal"
 	hfInformers "github.com/hobbyfarm/gargantua/v3/pkg/client/informers/externalversions"
+	authnpb "github.com/hobbyfarm/gargantua/v3/protos/authn"
+	authrpb "github.com/hobbyfarm/gargantua/v3/protos/authr"
 	environmentpb "github.com/hobbyfarm/gargantua/v3/protos/environment"
+	scheduledeventpb "github.com/hobbyfarm/gargantua/v3/protos/scheduledevent"
 )
 
 var (
@@ -32,6 +35,20 @@ func main() {
 
 	crd.InstallCrds(environmentservice.EnvironmentCRDInstaller{}, cfg, "environment")
 
+	services := []microservices.MicroService{
+		microservices.AuthN,
+		microservices.AuthR,
+		microservices.ScheduledEvent,
+	}
+	connections := microservices.EstablishConnections(services, serviceConfig.ClientCert)
+	for _, conn := range connections {
+		defer conn.Close()
+	}
+
+	authnClient := authnpb.NewAuthNClient(connections[microservices.AuthN])
+	authrClient := authrpb.NewAuthRClient(connections[microservices.AuthR])
+	scheduledEventClient := scheduledeventpb.NewScheduledEventSvcClient(connections[microservices.ScheduledEvent])
+
 	gs := microservices.CreateGRPCServer(serviceConfig.ServerCert.Clone())
 
 	es := environmentservice.NewGrpcEnvironmentServer(hfClient, hfInformerFactory)
@@ -43,6 +60,19 @@ func main() {
 	go func() {
 		defer wg.Done()
 		microservices.StartGRPCServer(gs, serviceConfig.EnableReflection)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		environmentServer := environmentservice.NewEnvironmentServer(
+			authnClient,
+			authrClient,
+			scheduledEventClient,
+			es,
+		)
+		microservices.StartAPIServer(environmentServer)
 	}()
 
 	hfInformerFactory.Start(stopCh)
