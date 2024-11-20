8e336c1c7cc116276c99059972f50fa0a02780e0
Eamon Bauman <eamon@eamonbauman.com>
2024-11-20 17:35:39 -0600

    wire up new stuff into apiserver

--- v4/pkg/server/server.go
+++ v4/pkg/server/server.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"context"
 	"github.com/hobbyfarm/gargantua/v4/pkg/authentication"
-	"github.com/hobbyfarm/gargantua/v4/pkg/authentication/token"
+	"github.com/hobbyfarm/gargantua/v4/pkg/authentication/authenticators"
+	"github.com/hobbyfarm/gargantua/v4/pkg/authentication/authenticators/cert"
+	"github.com/hobbyfarm/gargantua/v4/pkg/authentication/authenticators/token"
 	"github.com/hobbyfarm/gargantua/v4/pkg/authorization"
 	"github.com/hobbyfarm/gargantua/v4/pkg/openapi/hobbyfarm_io"
 	"github.com/hobbyfarm/gargantua/v4/pkg/scheme"
@@ -14,16 +17,23 @@ import (
 	"k8s.io/apimachinery/pkg/version"
 	"k8s.io/apiserver/pkg/registry/rest"
 	apiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/apiserver/pkg/server/options"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+type KubernetesServerConfig struct {
+	Client                client.WithWatch
+	ForceStorageNamespace string
+	CACertBundle          string
+}
+
 // NewKubernetesServer creates a new hobbyfarm-api server backed by a remote Kubernetes cluster.
 // client is a k8s client with watch capabilities.
-// forceStorageNamespace is the namespace in the remote k8s cluster in which all resources are stored.
+// ForceStorageNamespace is the namespace in the remote k8s cluster in which all resources are stored.
 // It is necessary to pass a namespace so that the server can force objects into the proper namespace
 // and not leak resource upon querying.
-func NewKubernetesServer(kclient client.WithWatch, forceStorageNamespace string) (*server.Server, error) {
-	v4alpha1Storage := kubernetes.V4Alpha1Storages(kclient, forceStorageNamespace)
+func NewKubernetesServer(ctx context.Context, config *KubernetesServerConfig) (*server.Server, error) {
+	v4alpha1Storage := kubernetes.V4Alpha1Storages(config.Client, config.ForceStorageNamespace)
 
 	v4alpha1ApiGroups, err := V4Alpha1APIGroups(v4alpha1Storage)
 	if err != nil {
@@ -35,14 +45,19 @@ func NewKubernetesServer(kclient client.WithWatch, forceStorageNamespace string)
 		return nil, err
 	}
 
-	authenticator := token.NewGenericGeneratorValidator(kclient)
+	certAuthenticatior, err := cert.NewCertAuthenticator(config.CACertBundle)
+	if err != nil {
+		return nil, err
+	}
+
+	// authenticator := token.NewGenericGeneratorValidator(Client)
+	authenticator := authenticators.NewChainAuthenticator(
+		certAuthenticatior,
+		token.NewGenericGeneratorValidator(config.Client))
 
 	authorizer := authorization.NewAuthorizer(v4alpha1Storage["rolebindings"],
 		v4alpha1Storage["roles"], "/auth/.*/login")
 
-	if err != nil {
-		return nil, err
-	}
 	svr, err := server.New(&server.Config{
 		Name:                         "hobbyfarm-api",
 		Version:                      "v4alpha1",
@@ -58,27 +73,42 @@ func NewKubernetesServer(kclient client.WithWatch, forceStorageNamespace string)
 		RemoteKubeConfigFileOptional: true,
 		IgnoreStartFailure:           false,
 		Middleware:                   nil,
-		Authenticator:                authenticator,
-		Authorization:                authorizer,
-		OpenAPIConfig:                hobbyfarm_io.GetOpenAPIDefinitions,
-		APIGroups:                    storage,
-		PostStartFunc:                nil,
-		SupportAPIAggregation:        false,
-		ReadinessCheckers:            nil,
+		Authentication: &options.DelegatingAuthenticationOptions{
+			SkipInClusterLookup:          true,
+			RemoteKubeConfigFileOptional: true,
+			ClientCert: options.ClientCertAuthenticationOptions{
+				ClientCA: config.CACertBundle,
+			},
+		},
+		Authenticator:         authenticator,
+		Authorization:         authorizer,
+		OpenAPIConfig:         hobbyfarm_io.GetOpenAPIDefinitions,
+		APIGroups:             storage,
+		PostStartFunc:         nil,
+		SupportAPIAggregation: false,
+		ReadinessCheckers:     nil,
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO - A cache can be setup here by passing options to client.Options
-	// May want to investigate that if auth becomes bound by the API
-	authClient, err := client.New(svr.GenericAPIServer.LoopbackClientConfig, client.Options{
-		Scheme: scheme.Scheme,
-	})
+	caches, err := authentication.SetupAuthentication(ctx, svr.GenericAPIServer.LoopbackClientConfig,
+		svr.GenericAPIServer.Handler.NonGoRestfulMux)
 	if err != nil {
 		return nil, err
 	}
-	authentication.RegisterHandlers(authClient, svr.GenericAPIServer.Handler.NonGoRestfulMux)
+
+	if err := svr.GenericAPIServer.AddPostStartHook("auth-caches", func(_ apiserver.PostStartHookContext) error {
+		for _, cache := range caches {
+			if err := cache.Start(ctx); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
 
 	svr.GenericAPIServer.Version = &version.Info{
 		Major: "4",
@@ -89,7 +119,6 @@ func NewKubernetesServer(kclient client.WithWatch, forceStorageNamespace string)
 }
 
 func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[string]rest.Storage, error) {
-
 	providerStorage, err := registry.NewProviderStorage(storages["providers"],
 		storages["machinesets"], storages["machines"], storages["environments"])
 	if err != nil {
@@ -107,35 +136,48 @@ func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[strin
 		return nil, err
 	}
 
+	environmentStatusStorage := registry.NewEnvironmentStatusStorage(storages["environments"].Scheme(), storages["environments"])
+
 	machineSetStorage, err := registry.NewMachineSetStorage(storages["machinesets"])
 	if err != nil {
 		return nil, err
 	}
+
+	machineSetStatusStorage := registry.NewMachineSetStatusStorage(storages["machinesets"].Scheme(), storages["machinesets"])
 
 	machineStorage, err := registry.NewMachineStorage(storages["machines"])
 	if err != nil {
 		return nil, err
 	}
 
+	machineStatusStorage := registry.NewMachineStatusStorage(storages["machines"].Scheme(), storages["machines"])
+
 	machineClaimStorage, err := registry.NewMachineClaimStorage(storages["machineclaims"])
 	if err != nil {
 		return nil, err
 	}
+
+	machineClaimStatusStorage := registry.NewMachineClaimStatusStorage(storages["machineclaims"].Scheme(), storages["machineclaims"])
 
 	scheduledEventStorage, err := registry.NewScheduledEventStorage(storages["scheduledevents"])
 	if err != nil {
 		return nil, err
 	}
 
+	scheduledEventStatusStorage := registry.NewScheduledEventStatusStorage(storages["scheduledevents"].Scheme(), storages["scheduledevents"])
+
 	accessCodeStorage, err := registry.NewAccessCodeStorage(storages["accesscodes"])
 	if err != nil {
 		return nil, err
 	}
+	accessCodeStatusStorage := registry.NewAccessCodeStatusStorage(storages["accesscodes"].Scheme(), storages["accesscodes"])
 
 	sessionStorage, err := registry.NewSessionStorage(storages["sessions"])
 	if err != nil {
 		return nil, err
 	}
+
+	sessionStatusStorage := registry.NewSessionStatusStorage(storages["sessions"].Scheme(), storages["sessions"])
 
 	courseStorage, err := registry.NewCourseStorage(storages["courses"])
 	if err != nil {
@@ -147,6 +189,8 @@ func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[strin
 		return nil, err
 	}
 
+	otacStatusStorage := registry.NewOneTimeAccessCodeStatusStorage(storages["onetimeaccesscodes"].Scheme(), storages["onetimeaccesscodes"])
+
 	predefinedServiceStorage, err := registry.NewPredefinedServiceStorage(storages["predefinedservices"])
 	if err != nil {
 		return nil, err
@@ -157,6 +201,8 @@ func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[strin
 		return nil, err
 	}
 
+	progressStatusStorage := registry.NewProgressStatusStorage(storages["progresses"].Scheme(), storages["progresses"])
+
 	scenarioStorage, err := registry.NewScenarioStorage(storages["scenarios"])
 	if err != nil {
 		return nil, err
@@ -166,6 +212,8 @@ func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[strin
 	if err != nil {
 		return nil, err
 	}
+
+	scenarioStepStatusStorage := registry.NewScenarioStepStatusStorage(storages["scenariosteps"].Scheme(), storages["scenariosteps"])
 
 	scopeStorage, err := registry.NewScopeStorage(storages["scopes"])
 	if err != nil {
@@ -181,6 +229,8 @@ func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[strin
 	if err != nil {
 		return nil, err
 	}
+
+	userStatusStorage := registry.NewUserStatusStorage(storages["users"].Scheme(), storages["users"])
 
 	serviceAccountStorage, err := registry.NewServiceAccountStorage(storages["serviceaccounts"])
 	if err != nil {
@@ -207,30 +257,59 @@ func V4Alpha1APIGroups(storages map[string]strategy.CompleteStrategy) (map[strin
 		return nil, err
 	}
 
+	ldapConfigStorage, err := registry.NewLdapConfigStorage(storages["ldapconfigs"])
+	if err != nil {
+		return nil, err
+	}
+
+	ldapConfigStatusStorage, err := registry.NewLdapConfigStatusStorage(storages["ldapconfigs"].Scheme(), storages["ldapconfigs"])
+	if err != nil {
+		return nil, err
+	}
+
+	groupStorage, err := registry.NewGroupStorage(storages["groups"])
+	if err != nil {
+		return nil, err
+	}
+
 	stores := map[string]rest.Storage{
-		"providers":          providerStorage,
-		"machinetemplates":   machineTemplateStorage,
-		"environments":       environmentStorage,
-		"machinesets":        machineSetStorage,
-		"machines":           machineStorage,
-		"machineclaims":      machineClaimStorage,
-		"scheduledevents":    scheduledEventStorage,
-		"accesscodes":        accessCodeStorage,
-		"sessions":           sessionStorage,
-		"courses":            courseStorage,
-		"onetimeaccesscodes": otacStorage,
-		"predefinedservices": predefinedServiceStorage,
-		"progresses":         progressStorage,
-		"scenarios":          scenarioStorage,
-		"scenariosteps":      scenarioStepStorage,
-		"scopes":             scopeStorage,
-		"settings":           settingStorage,
-		"users":              userStorage,
-		"serviceaccounts":    serviceAccountStorage,
-		"secrets":            secretStorage,
-		"configmaps":         configMapStorage,
-		"roles":              roleStorage,
-		"rolebindings":       roleBindingStorage,
+		"providers":                 providerStorage,
+		"machinetemplates":          machineTemplateStorage,
+		"environments":              environmentStorage,
+		"environments/status":       environmentStatusStorage,
+		"machinesets":               machineSetStorage,
+		"machinesets/status":        machineSetStatusStorage,
+		"machines":                  machineStorage,
+		"machines/status":           machineStatusStorage,
+		"machineclaims":             machineClaimStorage,
+		"machineclaims/status":      machineClaimStatusStorage,
+		"scheduledevents":           scheduledEventStorage,
+		"scheduledevents/status":    scheduledEventStatusStorage,
+		"accesscodes":               accessCodeStorage,
+		"accesscodes/status":        accessCodeStatusStorage,
+		"sessions":                  sessionStorage,
+		"sessions/status":           sessionStatusStorage,
+		"courses":                   courseStorage,
+		"onetimeaccesscodes":        otacStorage,
+		"onetimeaccesscodes/status": otacStatusStorage,
+		"predefinedservices":        predefinedServiceStorage,
+		"progresses":                progressStorage,
+		"progresses/status":         progressStatusStorage,
+		"scenarios":                 scenarioStorage,
+		"scenariosteps":             scenarioStepStorage,
+		"scenariosteps/status":      scenarioStepStatusStorage,
+		"scopes":                    scopeStorage,
+		"settings":                  settingStorage,
+		"users":                     userStorage,
+		"users/status":              userStatusStorage,
+		"serviceaccounts":           serviceAccountStorage,
+		"secrets":                   secretStorage,
+		"configmaps":                configMapStorage,
+		"roles":                     roleStorage,
+		"rolebindings":              roleBindingStorage,
+		"ldapconfigs":               ldapConfigStorage,
+		"ldapconfigs/status":        ldapConfigStatusStorage,
+		"groups":                    groupStorage,
 	}
 
 	return stores, nil
