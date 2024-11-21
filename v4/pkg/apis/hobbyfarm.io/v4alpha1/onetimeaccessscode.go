06c1d1260857e3d1747d444d52883870e539fd84
Eamon Bauman <eamon@eamonbauman.com>
2024-11-21 13:38:10 -0600

    feat: fixed otac spec and status (thanks Jan)

--- v4/pkg/apis/hobbyfarm.io/v4alpha1/onetimeaccessscode.go
+++ v4/pkg/apis/hobbyfarm.io/v4alpha1/onetimeaccessscode.go
@@ -23,20 +23,45 @@ type OneTimeAccessCodeList struct {
 }
 
 type OneTimeAccessCodeSpec struct {
-	// NotBefore is the timestamp before which a OneTimeAccessCode is invalid.
-	NotBefore *metav1.Time `json:"notBefore"`
+	// Scenarios is a list of the names of scenarios to which this AccessCode shall grant access.
+	Scenarios []string `json:"scenarios,omitempty"`
 
-	// NotAfter is the timestamp after which a OneTimeAccessCode is invalid.
-	NotAfter *metav1.Time `json:"notAfter"`
+	// Courses is a list of the names of Courses to which this AccessCode shall grant access.
+	Courses []string `json:"courses,omitempty"`
 
-	// User is the object name of the user for whom the OneTimeAccessCode is intended.
-	User string `json:"user"`
+	// ScheduledEvents is a list of the names of ScheduledEvents to which this AccessCode shall grant access.
+	ScheduledEvents []string `json:"scheduledEvents,omitempty"`
+
+	// MachineSets is a list of the names of MachineSets to which this AccessCode shall grant access.
+	MachineSets []string `json:"machineSets,omitempty"`
+
+	// NotBefore is a timestamp that defines the point in time after which an AccessCode may be used.
+	// If defined, the AccessCode may not be used before this timestamp. If nil, an AccessCode is considered valid
+	// immediately after creation - provided NotAfter has not passed if set.
+	NotBefore *metav1.Time `json:"notBefore,omitempty"`
+
+	// NotAfter is a timestamp that defines the point in time after which an AccessCode may NOT be used.
+	// If defined, the AccessCode may not be used after this timestamp. If nil, an AccessCode is considered valid
+	// forever - provided NotBefore has passed if set.
+	NotAfter *metav1.Time `json:"notAfter,omitempty"`
+
+	// AccessDuration defines how long the code will remain active *after redemption*.
+	// In other words, from the moment a user redeems an OTAC, this field determines for how long they
+	// shall have access to the resources described in this resource (courses, events, etc.).
+	AccessDuration *metav1.Duration `json:"accessDuration,omitempty"`
 }
 
 type OneTimeAccessCodeStatus struct {
 	// Redeemed is the timestamp marking when the OneTimeAccessCode was redeemed.
 	// nil if not redeemed.
 	Redeemed *metav1.Time `json:"redeemed,omitempty"`
+
+	// Expires is the timestamp marking when the OneTimeAccessCode shall stop providing
+	// access to resources. This value is status.redeemed + spec.accessDuration.
+	AccessExpires *metav1.Time `json:"accessExpires,omitempty"`
+
+	// User is the object name of the User who has consumed this OTAC.
+	User string `json:"user"`
 }
 
 func (c OneTimeAccessCode) NamespaceScoped() bool {
