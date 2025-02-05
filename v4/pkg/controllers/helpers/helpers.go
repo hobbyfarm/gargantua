7de2d3d36ea7b33f202f8ea275173bac6a2f2a1a
Eamon Bauman <eamon@eamonbauman.com>
2025-02-05 09:33:51 -0600

    feat: added helper for ReconcilerFunc

--- v4/pkg/controllers/helpers/helpers.go
+++ v4/pkg/controllers/helpers/helpers.go
@@ -6,6 +6,7 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"log/slog"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type Obj interface {
@@ -18,5 +19,13 @@ func UpdateStatus(ctx context.Context, client *client.Client, obj Obj) {
 	if err := client.UpdateStatus(ctx, obj.GetNamespace(), obj, obj, v1.UpdateOptions{}); err != nil {
 		slog.Error("updating status for object", "gvk", obj.GetObjectKind().GroupVersionKind().String(),
 			"objName", obj.GetName(), "error", err.Error())
+	}
+}
+
+func ReconcileFunc(f reconcile.Func) reconcile.Reconciler {
+	return struct {
+		reconcile.Func
+	}{
+		Func: f,
 	}
 }
