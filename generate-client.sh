08faf2a1b1f856b1bb8271bf2804cefa38a764de
Marcus Herz <marcus.herz@sva.de>
2025-02-18 15:26:11 +0100

    ignore kube_codegen.sh and fetch via curl in generate-client.sh

--- generate-client.sh
+++ generate-client.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# k8s.io/client-go v0.32.1 for kubernetes release 1.32
+KUBERNETES_CLIENT_GO_VERSION="v0.32.1"
+
 SCRIPT_PATH=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_PATH"
 
@@ -8,6 +11,8 @@ API_PATH="v3/pkg/apis"
 PROPERTY_PATH="v3/pkg/property"
 CLIENT_PATH="v3/pkg/client"
 
+echo "Downloading kube_codegen ${KUBERNETES_CLIENT_GO_VERSION}"
+curl -LOs https://raw.githubusercontent.com/kubernetes/code-generator/refs/tags/${KUBERNETES_CLIENT_GO_VERSION}/kube_codegen.sh
 source kube_codegen.sh
 
 mkdir -p ${CLIENT_PATH}/clientset
